f10d41afa0d391258e4a8e9bfe8b32af83a57d4f
wintermute3 <bright.tiger@gmail.com>
2021-04-24 22:12:08 -0400

    testing

--- zero.py
+++ zero.py
@@ -106,7 +106,7 @@ def cmd_off():
   return Feedback()
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0', port=80)
 
 #==============================================================================
 # end
